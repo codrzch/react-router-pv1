7c6ac437a12f9cab0457ffdfc30ac865c84f54d3
Zach James <codrzch@gmail.com>
2022-07-23 16:44:02 -0400

    Import page components into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
 const App = () => {
   return (
     <Router>
-      
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
     </Router>
   );
 };
